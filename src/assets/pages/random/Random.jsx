4fe599fae5e08538eb8b92d27b4380e599a6087c
elinefwd <elinefwd@gmail.com>
2023-12-29 22:35:38 +0100

    fridayevening

--- src/assets/pages/random/Random.jsx
+++ src/assets/pages/random/Random.jsx
@@ -1,21 +1,21 @@
-// import axios from "axios";
+ // import axios from "axios";
 
 function Random () {
 
-    async function fetchRecipe(){
-        try{
-        const result = await axios.get('https://api.edamam.com/api/recipes/v2');
-        console.log(result);
-    } catch (e){
-    console.error(e);
-        }
+    // async function fetchRecipe(){
+    //     try{
+    //         const result = await axios.get('https://api.edamam.com/api/recipes/v2');
+    //         console.log(result);
+    //     } catch (e){
+    //         console.error(e);
+    //     }
 
-    return (
-      <>
-            <p>
-                 hier hebben we een random optie die ook zonder inloggen of registreren te zien is
-            </p>
-</>)}
+        return (
+            <>
+                <p>
+                    hier hebben we een random optie die ook zonder inloggen of registreren te zien is
+                </p>
+            </>)}
 
 
 
